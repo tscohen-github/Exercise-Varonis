6aadee1d97e45ccc2dd8d14c162b518e9c28b328
tscohen-github <160016170+tscohen-github@users.noreply.github.com>
2024-02-15 09:35:45 +0200

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,18 +1,12 @@
-from pathlib import Path
 from github import Github
 import os
-from dotenv import load_dotenv
-
-ENV_PATH = os.path.join(Path(__file__).parent, ".env")
-
-load_dotenv(ENV_PATH)
 
 
-def is_private_repositories(username, password, repo_name):
-    if not (username and password):
-        print("Error: Personal Access Token not found. Please set GITHUB_TOKEN environment variable.")
+def is_private_repositories(token, repo_name):
+    if not token:
+        print("Error: Personal Access Token not found. Please set VARNIS_ACCESS_TOKEN environment variable.")
         exit()
-    account = Github(username, password)
+    account = Github(token)
     repo = account.get_repo(repo_name)
     if not repo.private:
         print(f"The repository '{repo_name}' is currently PUBLIC. Changing to PRIVATE...")
@@ -22,7 +16,6 @@ def is_private_repositories(username, password, repo_name):
         print(f"The repository '{repo_name}' is already PRIVATE.")
 
 
-g_username = os.getenv('USER_NAME')
-g_password = os.getenv('PASSWORD')
+g_access_token = os.getenv('VARNIS_ACCESS_TOKEN')
 g_repo_name = os.getenv('REPO_NAME')
-is_private_repositories(g_username, g_password, g_repo_name)
+is_private_repositories(g_access_token, g_repo_name)
