eba624d17e18dafc1ec1193e6556caa251be75a6
TsofiaCohen <tsofia.c@cybord.ai>
2024-02-15 02:59:54 +0200

    push

--- python_code.py
+++ python_code.py
@@ -8,20 +8,21 @@ ENV_PATH = os.path.join(Path(__file__).parent, ".env")
 load_dotenv(ENV_PATH)
 
 
-def is_private_repositories(token, repo_name):
-    if not token:
+def is_private_repositories(username, password, repo_name):
+    if not (username and password):
         print("Error: Personal Access Token not found. Please set GITHUB_TOKEN environment variable.")
         exit()
-    account = Github(token)
-    repo = account.get_repo("tsofia-git/Exercise-Varonis-New")
+    account = Github(username, password)
+    repo = account.get_repo(repo_name)
     if not repo.private:
         print(f"The repository '{repo_name}' is currently PUBLIC. Changing to PRIVATE...")
-        # repo.edit(private=True)
+        repo.edit(private=True)
         print(f"The repository '{repo_name}' has been successfully changed to PRIVATE.")
     else:
         print(f"The repository '{repo_name}' is already PRIVATE.")
 
 
-g_token = os.getenv('GITHUB_TOKEN')
+g_username = os.getenv('USER_NAME')
+g_password = os.getenv('PASSWORD')
 g_repo_name = os.getenv('REPO_NAME')
-is_private_repositories(g_token, g_repo_name)
+is_private_repositories(g_username, g_password, g_repo_name)
